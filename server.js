8a1fb8a3c0d1a1aac18674faa59ca1e97b177b2d
Gumbx <gustavo.bonassa1@gmail.com>
2019-05-06 16:13:27 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+    res.render('index.html');
+});
+
+let messages = [];
+
+var userCount = 0;
+io.on('connection', socket => {
+    userCount++;//conta usuarios online
+    socket.on('disconnect', function() {
+        userCount--;
+        io.sockets.emit('userCount', userCount);
+    });
+    io.sockets.emit('userCount', userCount);
+
+    console.log(`Usuarios online: ${userCount}`);
+
+    console.log(`Socket conectado: ${socket.id}`);
+
+    socket.emit('previousMessages', messages);//pega mensagens anteriores
+
+    socket.on('sendMessage', data => {//manda as mensagens para todos
+        messages.push(data);//pega as msg do index
+        socket.broadcast.emit('receivedMessage', data);
+    });
+});
+
+server.listen(3000);
