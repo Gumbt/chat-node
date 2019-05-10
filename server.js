79a003332d478e9747301799c83628dbf59cd1b0
Gumbx <gustavo.bonassa1@gmail.com>
2019-05-10 13:52:25 -0300

    Comandos

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ io.on('connection', socket => {
 
     socket.on("join", ob => {
         if(ob.atv == 0){
-            io.sockets.emit("update", ob.user + " entrou no server.")
+            io.sockets.emit("update", ob.user + " entrou no server.");
             messages.push({author:'Server',message:ob.user + " entrou no server."});
         }
         people.push(ob.user);
@@ -39,12 +39,29 @@ io.on('connection', socket => {
         console.log(`Usuarios online: ${people.length}`);
         console.log(`Socket conectado: ${socket.id}`);
     });
-    
+    socket.on('comandos', data => {
+        messages.push(data);
+        cont=0;
+        if(data.message=="/limpachat"){
+            socket.emit("update", data.author + " limpou o chat.");
+        }
+        if(data.message=="/comandos"){
+            socket.emit("update", "<br><b>/limpachat</b> - Limpa o chat<br><b>/gumb</b> - Mensagem");
+        }
+        if(data.message=="/gumb"){
+            var msg = {author:'Server',message:"Gumb é incrivel"};
+            io.sockets.emit("update", msg.message);
+            messages.push(msg);
+        }else{
+            socket.emit("update", "Comando não encontrado, digite <b>/comandos</b> para ver os comandos");
+        }
+
+    })
 
     socket.on('sendMessage', data => {//manda as mensagens para todos
         messages.push(data);//pega as msg do index
         socket.broadcast.emit('receivedMessage', data);
-    });''
+    });
 });
 
 server.listen(process.env.PORT || 3000);
